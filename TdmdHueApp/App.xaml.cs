f36c1f15109b1f2aebe9dc78258972d441843484
Luuk <luukboerman@hotmail.com>
2024-12-06 16:39:48 +0100

    Add project files.

---
+++ TdmdHueApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TdmdHueApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ TdmdHueApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TdmdHueApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
