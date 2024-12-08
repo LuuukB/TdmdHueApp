d1c7de4a800aa5ee9df2dee07776cde1d270f02b
Luuk <luukboerman@hotmail.com>
2024-12-08 16:21:45 +0100

    couple tests added

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -20,14 +20,49 @@ namespace UnitTestProject
             Assert.Equal(10000, lamp1.Hue);
         }
 
+        [Fact] 
+        public void EqualLampTest() {
+            Lamp lamp1 = new(1, true, 20, 200, 2000);
+            Lamp lamp2 = new(2, false, 10, 100, 1000);
+            Lamp lamp3 = new(1, true, 20, 200, 2000);
+            Lamp lamp4 = new(1, false, 20, 200, 2000);
+            Lamp lamp5 = new(1, true, 10, 200, 2000);
+            Lamp lamp6 = new(1, true, 20, 100, 2000);
+            Lamp lamp7 = new(1, true, 20, 200, 1000);
+
+            Assert.True(lamp1.Equals(lamp3));
+            Assert.False(lamp1.Equals(lamp2));
+            Assert.False(lamp1.Equals(lamp4));
+            Assert.False(lamp1.Equals(lamp5));
+            Assert.False(lamp1.Equals(lamp6));
+            Assert.False(lamp1.Equals(lamp7));
+        }
+
+        [Fact]
+        public void GetHasCodeTest()
+        {
+
+            var lamp = new Lamp (1, true, 100, 50, 200 );
+            var lamp2 = new Lamp (1, true, 100, 50, 200 );
+            var lamp3 = new Lamp (2, false, 50, 25, 100 );
+
+            var hash1 = lamp.GetHashCode();
+            var sameAsHash1 = lamp.GetHashCode();
+            var hash2 = lamp2.GetHashCode();
+            var hash3 = lamp3.GetHashCode();
+
+            Assert.Equal(hash1, sameAsHash1); // 2x hetzelfde hasen zou zelfde hash moeten geven
+            Assert.Equal(hash1, hash2); //verschillende objecten met zelfde inhoud zou zelfde hash moeten geven
+            Assert.NotEqual(hash1, hash3); // verschillende objecten met verschillende inhoud zou verschillende hash moeten geven
+
+        }
+
        
 
-        [Fact] public void Test2() {
-
-
+        [Fact] public void ExtractUsernameTest() {
 
             var mockPreferences = new Mock<IPreferences>();
-            var testJson = " [ {  \"success\": {\r\n               \"username\": \"1028d66426293e821ecfd9ef1a0731df\"\r\n             }\r\n           }\r\n ]";
+            var testJson = " [ { \"success\": { \"username\": \"1028d66426293e821ecfd9ef1a0731df\" } } ]";
 
             var extractUsername = new ExtractUsername(mockPreferences.Object);
 
